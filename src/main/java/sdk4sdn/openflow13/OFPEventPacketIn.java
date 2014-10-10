688d8cdbde6dec43009386654f7eca85ec649123
Philipp Aeschlimann (paeschli) <philipp.aeschlimann@gmail.com>
2014-10-10 10:09:32 +0200

    - removed not need lib

--- src/main/java/sdk4sdn/openflow13/OFPEventPacketIn.java
+++ src/main/java/sdk4sdn/openflow13/OFPEventPacketIn.java
@@ -34,7 +34,6 @@
 package sdk4sdn.openflow13;
 import ro.fortsoft.pf4j.ExtensionPoint;
 import sdk4sdn.Network;
-import sdk4sdn.openflow13.OpenFlow;
 
 //FIXME: Create a new subpackge for the OFP implementation
 
